2f1e890a942996003c98609636db790613d3fbc8
Dean <stevandeanachmad1@gmail.com>
2023-06-18 07:13:53 +0700

    Update : ui statistik

--- fe-ukt/pages/admin/index.jsx
+++ fe-ukt/pages/admin/index.jsx
@@ -10,7 +10,7 @@ const ChartBar = ({ name, percentages }) => {
 
     useEffect(() => {
         const startAnimation = () => {
-            setAnimationStarted (true);
+          setAnimationStarted(true);
         };
     
         setTimeout(startAnimation, 1000); // Waktu penundaan sebelum animasi dimulai (dalam milidetik)
@@ -21,40 +21,32 @@ const ChartBar = ({ name, percentages }) => {
             <div className="w-1/4">
                 <span 
                     // className={` ${
-                    //     animationStarted ? '' : 'animate-opacity-from-left text-white'
-                    // } ' text-white text-lg font-semibold tracking-wider'`}
+                    //     animationStarted ? '' : 'animate-slide-in-right transition-opacity duration-3000 text-white'
+                    // } 'text-white text-lg font-semibold tracking-wider'`}
                     className="text-white text-lg font-semibold tracking-wider"
                 >{name}
                 </span>
             </div>
             <div className="w-3/4">
                 <div className="flex h-6 rounded-full text-white">
-                    <div className='text-center bg-red' style={{ width: `${percentages.red}%` }}>
-                        <div
-                            className={` ${
-                                animationStarted ? '' : 'animate-opacity-from-left'
-                            } " text-center bg-red rounded-l-md text-transparent "`}>
-                            {percentages.red}
-                        </div>
-                        <h1 className={` ${animationStarted ? '' : 'animate-opacity-from-left'} ' text-red  '`}>{percentages.red + '%'}</h1>
+                    <div
+                        className={` ${
+                            animationStarted ? '' : 'animate-opacity-from-left'
+                        } "text-center bg-red rounded-l-md"`}
+                        style={{ width: `${percentages.red}%`}}
+                    >{percentages.red + '%'}
                     </div>
-                    <div className='text-center bg-yellow' style={{ width: `${percentages.yellow}%` }}>
-                        <div
-                            className={` ${
-                                animationStarted ? '' : 'animate-opacity-from-left'
-                            } " text-center bg-yellow rounded-l-md text-transparent "`}>
-                            {percentages.yellow}
-                        </div>
-                        <h1 className={` ${animationStarted ? '' : 'animate-opacity-from-left'} ' text-yellow  '`}>{percentages.yellow + '%'}</h1>
+                    <div
+                        className="text-center bg-yellow"
+                        style={
+                            { width: `${percentages.yellow}%`}
+                        }
+                    >{percentages.yellow + '%'}
                     </div>
-                    <div className='text-center bg-green' style={{ width: `${percentages.green}%` }}>
-                        <div
-                            className={` ${
-                                animationStarted ? '' : 'animate-opacity-from-left'
-                            } " text-center bg-green rounded-l-md text-transparent "`}>
-                            {percentages.green}
-                        </div>
-                        <h1 className={` ${animationStarted ? '' : 'animate-opacity-from-left'} ' text-green  '`}>{percentages.green + '%'}</h1>
+                    <div
+                        className="text-center bg-green rounded-r-md"
+                        style={{ width: `${percentages.green}%`}}
+                    >{percentages.green + '%'}
                     </div>
                 </div>
             </div>
@@ -118,7 +110,7 @@ const index = () => {
                         <h1 className='text-2xl tracking-wider uppercase font-bold'>Dashboard</h1>
                         
                         {/* download button */}
-                        <div className="bg-purple rounded-md px-5 py-2 flex items-center gap-x-2 w-48 justify-center">
+                        <div className="bg-purple rounded-md px-5 py-2 flex items-center gap-x-2">
                             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M15.75 11.25V14.25C15.75 14.6478 15.592 15.0294 15.3107 15.3107C15.0294 15.592 14.6478 15.75 14.25 15.75H3.75C3.35218 15.75 2.97064 15.592 2.68934 15.3107C2.40804 15.0294 2.25 14.6478 2.25 14.25V11.25" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                                 <path d="M5.25 7.5L9 11.25L12.75 7.5" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
@@ -134,16 +126,19 @@ const index = () => {
                         {/* wrapper chart bar description */}
                         <div id='animationElement' className="grid grid-cols-3 gap-x-8 text-white">
                             <div className="inline-flex items-center gap-x-2">
-                                <div className="bg-red h-5 w-5"></div>
+                                <div className="bg-red h-5 w-5">
+                                </div>
                                 <h1 className='text-lg font-semibold'>Kurang</h1>
                             </div>
                             <div className="inline-flex items-center gap-x-2">
-                                <div className="bg-yellow h-5 w-5"></div>
-                                <h1 className='text-lg font-semibold'>Baik</h1>
+                                <div className="bg-yellow h-5 w-5">
+                                </div>
+                                <h1 className='text-lg font-semibold'>Kurang</h1>
                             </div>
                             <div className="inline-flex items-center gap-x-2">
-                                <div className="bg-green h-5 w-5"></div>
-                                <h1 className='text-lg font-semibold'>Cukup</h1>
+                                <div className="bg-green h-5 w-5">
+                                </div>
+                                <h1 className='text-lg font-semibold'>Kurang</h1>
                             </div>
                         </div>
 
@@ -171,17 +166,11 @@ const index = () => {
                     </div>
 
                     {/* wrapper chart bar */}
-                    <div className="bg-navy p-7 rounded-lg space-y-8 mb-8">
-                        <div className="text-white text-3xl font-semibold tracking-wider text-center mb-6 uppercase">
-                            Ranting Trenggalek
-                        </div>
+                    <div className="bg-navy p-7 rounded-lg space-y-4">
+                        <div className="text-white text-2xl font-semibold tracking-wider text-center mb-6 uppercase">Ranting Trenggalek</div>
                         {data.map((item, index) => (
                             <ChartBar key={index} name={item.name} percentages={item.percentages} />
                         ))}
-                    </div>
-                    <div className="bg-navy p-7 rounded-lg space-y-4">
-                        <h1 className='text-center text-white text-3xl font-semibold tracking-wider'>Catatan</h1>
-                        <textarea className='w-full rounded-md p-2 text-xl bg-gray text-white' name="" id="" cols="30" rows="10"></textarea>
                     </div>
                 </div>
                 {/* akhir konten utama */}
